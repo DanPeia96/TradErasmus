9b888338a9db723fcb8e5071a8c02983371ae53f
Dan <36074341+DanPeia96@users.noreply.github.com>
2021-05-28 01:51:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.get('/publications/:city', control.getPublicationsByCity)
 app.post('/registro', control.registrar)
 app.post('/login', control.login)
 
-const PORT = 8090
+const PORT = 8080
 app.listen(PORT, _ => console.log(`Servidor web escuchando en puerto ${PORT}`))
